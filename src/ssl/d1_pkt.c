264d74c945947315d0667da5d211ad0a3e5048da
Marc-André Moreau <marcandre.moreau@gmail.com>
2014-04-26 15:20:30 -0400

    ssl/d1_pkt.c: fix Linux build

--- src/ssl/d1_pkt.c
+++ src/ssl/d1_pkt.c
@@ -115,7 +115,16 @@
 
 #include <stdio.h>
 #include <errno.h>
+
+#ifdef __OpenBSD__
 #include <machine/endian.h>
+#else
+#include <endian.h>
+#define _BYTE_ORDER __BYTE_ORDER
+#define _BIG_ENDIAN __BIG_ENDIAN
+#define _LITTLE_ENDIAN __LITTLE_ENDIAN
+#endif
+
 #include "ssl_locl.h"
 #include <openssl/evp.h>
 #include <openssl/buffer.h>
